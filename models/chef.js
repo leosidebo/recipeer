12e1d5f9b26c81bd488191f4074bdce06206924b
Leo Sidebo <54834547+leosidebo@users.noreply.github.com>
2020-02-13 14:12:22 +0100

    Updated Chef Model

--- models/chef.js
+++ models/chef.js
@@ -1,14 +1,29 @@
 const mongoose = require('mongoose')
+const Role = require('./role')
 
 const chefSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
     },
-    professional: {
-        type: Boolean,
+    role: {
+        type: String,
         required: true
     },
+    hoursPerWeek: {
+        type: Number,
+        required: true
+    },
+    shift: {
+        type: String, 
+        required: true
+    },
+    yearsWorked: { // Consider making type: Date instead
+        type: Number,
+        required: true
+    }
 })
+
+// Should also have a restaurant associated with them
 
 module.exports = mongoose.model('Chef', chefSchema)
