d290187a0bf019a6180f1bd4a9ad98d6db4eeaa8
Leo Sidebo <abletoirper@gmail.com>
2020-02-16 21:11:08 +0100

    Added Mocha Tests for Role POST Request, Not Currently Working

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-}
+// if (process.env.NODE_ENV != 'production') {
+//     require('dotenv').config()
+// }
+
+require('dotenv').config()
 
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const indexRouter = require('./routes/index')
 const chefRouter = require('./routes/chefs')
@@ -25,10 +28,17 @@ app.use('/chefs', chefRouter)
 app.use('/roles', roleRouter)
 app.use('/shifts', shiftRouter)
 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
-const db = mongoose.connection
-db.on('error', error => console.log(error))
-db.once('open', () => console.log('Connected to Mongoose'))
+connect()
 
-app.listen(process.env.PORT || 4000)
+function connect() {
+    mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+    const db = mongoose.connection
+    db.on('error', error => console.log(error))
+    db.once('open', () => console.log('Connected to Mongoose'))
+}
+
+function disconnect() {
+    return mongoose.disconnect()
+}
+
+app.listen(4000)
